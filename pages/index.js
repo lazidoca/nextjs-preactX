1d544da432e361dc567af75e297331fec9b24fd4
^ <31332276+lazidoca@users.noreply.github.com>
2019-10-14 14:51:16 +0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import { h } from 'preact'
+import { FaBeer } from 'react-icons/fa';
+
+
+const Index = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <h3> Lets go for a <FaBeer />? </h3>
+        </div>
+    )
+}
+
+export default Index
